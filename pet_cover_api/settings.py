bb9f385096a5653144dcb68f9439c85aad6b128c
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-09-07 18:40:32 +0300

    chore: updated list of installed apps

--- pet_cover_api/settings.py
+++ pet_cover_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pet_insurance.apps.Pet_insuranceConfig', 
+
 ]
 
 MIDDLEWARE = [
