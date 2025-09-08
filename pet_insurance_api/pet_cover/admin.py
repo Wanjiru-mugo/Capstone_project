c45b7264a112f88c9911517304fdbabbfbd47476
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-09-08 22:45:47 +0300

    fix: fixed NameError in admin file

--- pet_insurance_api/pet_cover/admin.py
+++ pet_insurance_api/pet_cover/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+#Register your models here.
+from .models import InsuranceCompany, VetClinic, Plan, Benefit, ExclusiveOf
+
+admin.site.register(InsuranceCompany)
+admin.site.register(VetClinic)
+admin.site.register(Plan)
+admin.site.register(Benefit)
+admin.site.register(ExclusiveOf)
