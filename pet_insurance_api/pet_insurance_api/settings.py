7658e572b40ab289e24d8a636e8a31aa7872ebf1
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-09-09 18:33:06 +0300

    fix:TypeError bug on TEMPLATES setting

--- pet_insurance_api/pet_insurance_api/settings.py
+++ pet_insurance_api/pet_insurance_api/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'pet_insurance_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [str(BASE_DIR / 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
