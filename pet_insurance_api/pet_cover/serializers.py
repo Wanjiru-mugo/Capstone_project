f97ef71019e521e65b67237077dab21abcf772cf
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-09-09 13:14:58 +0300

    feat:add serializers for all models

---
+++ pet_insurance_api/pet_cover/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from .models import InsuranceCompany, Benefit, Plan, ExclusiveOf, VetClinic
+
+class BenefitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Benefit
+        fields = ['name', 'description']
+
+class ExclusiveOfSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExclusiveOf
+        fields = ['name', 'description']
+
+class VetClinicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VetClinic
+        fields = ['name', 'address', 'contact_phone']
+
+class PlanSerializer(serializers.ModelSerializer):
+
+    #return plan API with related benefits, exclusions, vetservice
+    benefits = BenefitSerializer(many=True, read_only=True)
+    exclusions = ExclusiveOfSerializer(many=True, read_only=True)
+    vetservice = VetClinicSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Plan
+        fields = ['name', 'insurance_company', 'premium', 'deductible', 'annual_limit', 'waiting_period', 'copay', 'vet_clinic']
+
+    def validate(self, data):
+        #premium value should be less that annual_limit
+        premium = data.get('premium')
+        annual_limit = data.get('annual_limit')
+
+        if premium is not None and annual_limit is not None:
+            if premium >= annual_limit:
+                raise serializers.ValidationError("Premium must be less than the annual limit!")
+            
+class InsuranceCompanySerializer(serializers.ModelSerializer):
+
+    #return related plans
+    plans = PlanSerializer(many=True, read_only=True)
+    class Meta:
+        model = InsuranceCompany
+        fields = ['name', 'description', 'contact_email']
