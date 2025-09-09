cc4f3ab23075187dc1ccbbe3ea1861101302a5ab
Wanjiru-mugo <wanjirumugomary@gmail.com>
2025-09-09 05:29:01 +0300

    feat: create and customize admin interface

--- pet_insurance_api/pet_cover/admin.py
+++ pet_insurance_api/pet_cover/admin.py
@@ -3,8 +3,23 @@ from django.contrib import admin
 #Register your models here.
 from .models import InsuranceCompany, VetClinic, Plan, Benefit, ExclusiveOf
 
+class PlanAdmin(admin.ModelAdmin):
+    list_filter = ('insurance_company',)
+
+class BenefitAdmin(admin.ModelAdmin):
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+class ExclusiveOfAdmin(admin.ModelAdmin):
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+class VetClinicAdmin(admin.ModelAdmin):
+    list_filter = ('address',)
+    search_fields = ('address', 'name',)
+
 admin.site.register(InsuranceCompany)
-admin.site.register(VetClinic)
-admin.site.register(Plan)
-admin.site.register(Benefit)
-admin.site.register(ExclusiveOf)
+admin.site.register(VetClinic, VetClinicAdmin)
+admin.site.register(Plan, PlanAdmin)
+admin.site.register(Benefit, BenefitAdmin)
+admin.site.register(ExclusiveOf, ExclusiveOfAdmin)
